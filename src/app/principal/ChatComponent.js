943d0cc40e87459f8553c5f355e9c20daa04fffd
Agustín Avellaneda <agustin2051@gmail.com>
2023-08-11 18:44:51 -0300

    Act-Img

--- src/app/principal/ChatComponent.js
+++ src/app/principal/ChatComponent.js
@@ -90,7 +90,7 @@ const ChatComponent = () => {
           <div className="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg">
             <div className="h-20 w-20 rounded-full border overflow-hidden">
               <Image
-                src="https://avatars3.githubusercontent.com/u/2763884?s=128"
+                src="https://res.cloudinary.com/dcu06etml/image/upload/v1691789978/draig-page/examples/sdtk5u0m7d4fckiwbzgl.webp"
                 alt="Avatar"
                 className="h-full w-full"
               />
--- src/components/InfoData.js
+++ src/components/InfoData.js
@@ -2,19 +2,19 @@ export const infoTeam = [
   {
     Name: "Pablo Vegvarel",
 
-    TeamJob: "DETOI",
+    TeamJob: "CEO",
     TeamImg:
-      "https://res.cloudinary.com/dcu06etml/image/upload/v1688835318/profile-pic_2_pxpzfg.png",
-    SocialmediaOne: "",
-    SocialmediaTwo: "",
-    SocialmediaThree: "",
+      "https://res.cloudinary.com/dcu06etml/image/upload/v1691789971/draig-page/examples/y0i2elhu5r7d10f4ot4m.webp",
+    SocialmediaOne: "https://x.com/",
+    SocialmediaTwo: "https://google.com",
+    SocialmediaThree: "https://www.linkedin.com/",
   },
   {
     Name: "Avellaneda Agustín ",
 
     TeamJob: "DETOI",
     TeamImg:
-      "https://res.cloudinary.com/dcu06etml/image/upload/v1688835318/profile-pic_2_pxpzfg.png",
+      "https://res.cloudinary.com/dcu06etml/image/upload/v1691789985/draig-page/examples/n8ehlofzvooxe88tgfnh.webp",
     SocialmediaOne: "https://x.com/nitdraig",
     SocialmediaTwo: "https://agustin.top/",
 
@@ -25,21 +25,21 @@ export const infoTeam = [
   {
     Name: "Francisco Vega",
 
-    TeamJob: "DETOI",
+    TeamJob: "UX/UI Engineer",
     TeamImg:
-      "https://res.cloudinary.com/dcu06etml/image/upload/v1688835318/profile-pic_2_pxpzfg.png",
-    SocialmediaOne: "",
-    SocialmediaTwo: "",
-    SocialmediaThree: "",
+      "https://res.cloudinary.com/dcu06etml/image/upload/v1691789978/draig-page/examples/sdtk5u0m7d4fckiwbzgl.webp",
+    SocialmediaOne: "https://x.com/",
+    SocialmediaTwo: "https://google.com",
+    SocialmediaThree: "https://www.linkedin.com/",
   },
   {
     Name: "Ana Romero",
 
-    TeamJob: "DETOI",
+    TeamJob: "Front-End Engineer",
     TeamImg:
-      "https://res.cloudinary.com/dcu06etml/image/upload/v1688835318/profile-pic_2_pxpzfg.png",
-    SocialmediaOne: "",
-    SocialmediaTwo: "",
-    SocialmediaThree: "",
+      "https://res.cloudinary.com/dcu06etml/image/upload/v1691789973/draig-page/examples/gie2hf1lzsimoejmadvx.webp",
+    SocialmediaOne: "https://x.com/",
+    SocialmediaTwo: "https://google.com",
+    SocialmediaThree: "https://www.linkedin.com/",
   },
 ];
